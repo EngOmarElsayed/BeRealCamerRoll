de12e6b2af0093d55991c80e0a9dff43a20fc99d
Omar Elsayed <eng.omar.elsayed@hotmail.com>
2024-08-04 14:19:46 +0300

    Initial Commit

---
+++ BeRealCamerRoll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeRealCamerRoll
+//
+//  Created by Eng.Omar Elsayed on 04/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
