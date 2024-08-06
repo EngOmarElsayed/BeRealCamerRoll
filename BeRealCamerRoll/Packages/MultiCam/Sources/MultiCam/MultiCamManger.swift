f3e6d8602bf5d53b3213693f3bea050b338c4b92
Omar Elsayed <eng.omar.elsayed@hotmail.com>
2024-08-06 11:38:10 +0300

    created the MultiCamManger

--- BeRealCamerRoll/Packages/MultiCam/Sources/MultiCam/MultiCamManger.swift
+++ BeRealCamerRoll/Packages/MultiCam/Sources/MultiCam/MultiCamManger.swift
@@ -5,4 +5,17 @@
 //  Created by Eng.Omar Elsayed on 06/08/2024.
 //
 
-import Foundation
+import AVFoundation
+
+final class MultiCamManger {
+  private let multiCamSession = AVCaptureMultiCamSession()
+  
+  private var frontPreviewLayer: AVCaptureVideoPreviewLayer?
+  private var backPreviewLayer: AVCaptureVideoPreviewLayer?
+  
+  private var frontPreviewConnection: AVCaptureConnection?
+  private var backPreviewConnection: AVCaptureConnection?
+  
+  private var frontCameraInputPorts: AVCaptureDeviceInput.Port?
+  private var backCameraInputPorts: AVCaptureDeviceInput.Port?
+}
